92f781da942675376147c8b04de3fb618f8630b6
ubulina <ukokko@gmail.com>
2021-10-31 18:33:54 +0200

    Numeronarvaus-sovellus

--- App.js
+++ App.js
@@ -1,11 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState} from 'react';
+import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
+
+const theNumber = Math.floor(Math.random() * 100) + 1
 
 export default function App() {
+
+  const [notification, setNotification] = useState('Guess a number between 1-100')
+  const [guess, setGuess] = useState('')
+  const [numberOfGuesses, setNumberOfGuesses] = useState(1)
+
+  
+  const compareNumbers = () => {
+
+    setNumberOfGuesses(numberOfGuesses + 1)
+
+    if(guess > theNumber) {
+
+      
+      setNotification(`Your guess ${guess} is too high`)
+      
+      setGuess('')
+    }
+
+    else if (guess < theNumber) {
+
+      
+      setNotification(`Your guess ${guess} is too low`)
+      
+      setGuess('')
+
+    }
+
+    else {
+
+      if (numberOfGuesses === 1) {
+
+        Alert.alert(`You guessed the right number '${theNumber}' in ${numberOfGuesses} guess`)
+
+      }
+
+      else {
+
+        Alert.alert(`You guessed the right number '${theNumber}' in ${numberOfGuesses} guesses`)
+
+      }
+     
+      setNotification('Guess a number between 1-100')
+
+      setGuess('')
+      
+      setNumberOfGuesses(1)
+            
+
+    }
+
+  } 
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{notification}</Text>
+      <TextInput 
+        style={styles.input}
+        onChangeText={guess => setGuess(guess)}
+        value={guess}
+        keyboardType="numeric"
+      />
+      <Button onPress={compareNumbers} title="make guess" />      
+
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +81,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 30,
+    width: 50,
+    borderColor: 'gray',
+    borderWidth: 1
+  }
 });
